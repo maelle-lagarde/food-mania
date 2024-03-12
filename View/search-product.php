854fb950da3c54de69d04b1c422bc839285c53af
maelle-lagarde <maelle.lagarde@laplateforme.io>
2024-03-12 23:07:11 +0100

    search-product / design update

--- View/search-product.php
+++ View/search-product.php
@@ -8,23 +8,24 @@
     <title>Food Mania</title>
 </head>
 <body>
-    <h1>Display Product</h1>
+    <div class="wrapper">
+        <h1>Display Product</h1>
 
-        <?php
-            $response = file_get_contents('https://world.openfoodfacts.org/api/v2/product/3017624010701?fields=product_name,nutriscore_data');
+            <?php
+                $response = file_get_contents('https://world.openfoodfacts.org/api/v2/product/3017624010701?fields=product_name,nutriscore_data');
 
-            $data = json_decode($response, true);
+                $data = json_decode($response, true);
 
-            $product_name = $data['product']['product_name'];
-            $nutriscore_data = json_encode($data['product']['nutriscore_data']);
+                $product_name = $data['product']['product_name'];
+                $nutriscore_data = json_encode($data['product']['nutriscore_data']);
 
-            echo "<h2>$product_name</h2>";
-            echo "<p>$nutriscore_data</p>";
-            ?>
+                echo "<h2>$product_name</h2>";
+                echo "<p>$nutriscore_data</p>";
+                ?>
 
-    <script src="src/apiexterne/searchProduct.js"></script>
+        <script src="src/apiexterne/searchProduct.js"></script>
 
-    <a href="/food-mania/" class="home-button">Home</a>
-
+        <a href="/food-mania/" class="home-button">Home</a>
+    </div>
 </body>
 </html>
